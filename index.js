97a45910bd93b701b1557f1cd92a85fc4f11bd10
Timothy Sharkey <Techne3@gmail.com>
2019-10-31 12:35:18 -0600

    deploy update

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
+
 
 const server = express(); // creating an instance of an express server
 
+console.log('\n message', process.env.MSG)
+
 server.get('/', (req, res) => {
-  res.status(200).json({ hello: 'Web 23' });
+  res.status(200).json({ hello: process.env.MSG });
 });
 
 
 const port = process.env.PORT || 4000 
 
 server.listen(port, () => {
-  console.log('\n Server running on port 4000 \n');
+  console.log(`\n Server running on port ${port} \n`);
 });
 
